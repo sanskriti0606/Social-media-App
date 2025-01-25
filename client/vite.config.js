79a4580ef04eff556d84398fe0ae541e1b719dda
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2025-01-25 21:53:53 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative paths in production
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    mimeTypes: {
-      '.js': 'application/javascript', // Ensure JS files are served with the correct MIME type
-      '.jsx': 'application/javascript', // Add support for .jsx files if using React
-    },
-  },
-});
+  plugins: [react()]
+})
