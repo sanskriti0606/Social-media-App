a59d20c6c6c175197d5ef83f911befa847e1bb24
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2025-01-25 21:04:34 +0530

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,7 @@ import App from './App';
 import ThemeProvider from './context/theme';
 
 const httpLink = createHttpLink({
-  uri: 'https://social-media-app-5c0s.onrender.com/',
+  uri: 'https://social-media-app-5c0s.onrender.com/graphql',
   //uri: 'http://localhost:8080/'
 });
 
