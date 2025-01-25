a3157e5f9463f82e59170e79e2ae3b6b16c4dd24
unknown <sanskritichaudhary369@gmail.com>
2025-01-25 17:45:09 +0530

    add changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000, // Set the limit to 1 MB (1000 KB)
-  }
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 KiB
+  },
 });
