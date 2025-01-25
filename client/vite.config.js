04874f5565955a5cacf8ac73a76dfc380f9bf088
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2025-01-25 21:36:25 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-
 export default defineConfig({
   base: './', // Ensures relative paths in production
   build: {
     outDir: 'dist',
+  },
+  server: {
+    mimeTypes: {
+      '.js': 'application/javascript', // Make sure JS files are served with the correct MIME type
+    },
   },
 });
